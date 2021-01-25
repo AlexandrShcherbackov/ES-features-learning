5bbda8724795a918ff20b370153b93df999c9e67
Alexandr <jsprog2017@yandex.ru>
2021-01-25 09:40:45 +0300

    added html-webpack-plugin

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require("webpack");
+const path = require("path");
+
+//plugins
+const HTMLWebpackplugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    main: "./index.js",
+  },
+  output: {
+    filename: "[name].[hash].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [new HTMLWebpackplugin({
+    template: "./index.html",
+  })],
+};
